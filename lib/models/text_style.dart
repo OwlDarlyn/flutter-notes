d686d4e4b9611b72d8c4924878e45425be162baa
OwlDarlyn <imdarlyyn@gmail.com>
2023-03-30 16:52:47 +0300

    text style model

--- lib/models/text_style.dart
+++ lib/models/text_style.dart
@@ -1,1 +1,29 @@
 import 'package:flutter/material.dart';
+
+import '../models/app_color.dart';
+
+const TextStyle defaultMonthTextStyle = TextStyle(
+  color: AppColors.defaultMonthColor,
+  fontSize: Dimen.monthTextSize,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle defaultDateTextStyle = TextStyle(
+  color: AppColors.defaultDateColor,
+  fontSize: Dimen.dateTextSize,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle defaultDayTextStyle = TextStyle(
+  color: AppColors.defaultDayColor,
+  fontSize: Dimen.dayTextSize,
+  fontWeight: FontWeight.w500,
+);
+
+class Dimen {
+  Dimen._();
+
+  static const double dateTextSize = 15;
+  static const double dayTextSize = 11;
+  static const double monthTextSize = 11;
+}
