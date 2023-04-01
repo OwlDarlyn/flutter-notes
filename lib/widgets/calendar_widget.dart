5be20406f4398412972e5690f80394ca86d10341
OwlDarlyn <imdarlyyn@gmail.com>
2023-04-01 11:08:50 +0300

    calendar widget update

--- lib/widgets/calendar_widget.dart
+++ lib/widgets/calendar_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import 'package:table_calendar/table_calendar.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class CalendarWidget extends StatefulWidget {
   const CalendarWidget({super.key});
@@ -11,181 +11,296 @@ class CalendarWidget extends StatefulWidget {
 }
 
 class _CalendarWidgetState extends State<CalendarWidget> {
-  final DateTime _selectedDate = DateTime.now();
+  final todaysDate = DateTime.now();
+  var _focusedCalendarDate = DateTime.now();
+  final _initialCalendarDate = DateTime(2000);
+  final _lastCalendarDate = DateTime(2050);
+  DateTime? selectedCalendarDate;
+  final titleController = TextEditingController();
+  final descpController = TextEditingController();
 
-  int _selectedIndex = 0;
-  late int indexOfFirstDayMonth;
+  late Map<DateTime, List<MyEvents>> mySelectedEvents;
+
+  TextStyle dayStyle(FontWeight fontWeight) {
+    return const TextStyle(color: Colors.black87, fontWeight: FontWeight.bold);
+  }
+
+  Container taskList(
+      String title, String description, IconData iconImg, Color iconColor) {
+    return Container(
+      padding: const EdgeInsets.only(top: 20),
+      child: Row(
+        children: [
+          Icon(
+            iconImg,
+            color: Colors.black,
+            size: 30,
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  description,
+                  style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   void initState() {
-    indexOfFirstDayMonth = getIndexOfFirstDayInMonth(_selectedDate);
+    selectedCalendarDate = _focusedCalendarDate;
+    mySelectedEvents = {};
     super.initState();
-    setState(() {
-      _selectedIndex = indexOfFirstDayMonth +
-          int.parse(DateFormat('d').format(DateTime.now())) -
-          1;
-    });
+  }
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    descpController.dispose();
+    super.dispose();
+  }
+
+  List<MyEvents> _listOfDayEvents(DateTime dateTime) {
+    return mySelectedEvents[dateTime] ?? [];
+  }
+
+  _showAddEventDialog() async {
+    await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text('New Event'),
+              content: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  buildTextField(
+                      controller: titleController, hint: 'Enter Title'),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  buildTextField(
+                      controller: descpController, hint: 'Enter Description'),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    if (titleController.text.isEmpty &&
+                        descpController.text.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Please enter title & description'),
+                          duration: Duration(seconds: 3),
+                        ),
+                      );
+                      //Navigator.pop(context);
+                      return;
+                    } else {
+                      setState(() {
+                        if (mySelectedEvents[selectedCalendarDate] != null) {
+                          mySelectedEvents[selectedCalendarDate]?.add(MyEvents(
+                              eventTitle: titleController.text,
+                              eventDescp: descpController.text));
+                        } else {
+                          mySelectedEvents[selectedCalendarDate!] = [
+                            MyEvents(
+                                eventTitle: titleController.text,
+                                eventDescp: descpController.text)
+                          ];
+                        }
+                      });
+
+                      titleController.clear();
+                      descpController.clear();
+
+                      Navigator.pop(context);
+                      return;
+                    }
+                  },
+                  child: const Text('Add'),
+                ),
+              ],
+            ));
+  }
+
+  Widget buildTextField(
+      {String? hint, required TextEditingController controller}) {
+    return TextField(
+      controller: controller,
+      textCapitalization: TextCapitalization.words,
+      decoration: InputDecoration(
+        labelText: hint ?? '',
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.black, width: 1.5),
+          borderRadius: BorderRadius.circular(
+            10.0,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.black, width: 1.5),
+          borderRadius: BorderRadius.circular(
+            10.0,
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          shadowColor: Colors.transparent,
-          leading: const Icon(
-            Icons.arrow_back,
-            color: Colors.black,
-          ),
-          actions: const [
-            Padding(
-              padding: EdgeInsets.all(15),
-              child: Icon(
-                Icons.arrow_forward,
-                color: Colors.black,
-              ),
-            ),
-          ],
-          title: Column(
+        child: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () => _showAddEventDialog(),
+      ),
+      body: Container(
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: SingleChildScrollView(
+          child: Column(
             children: [
-              const Text(
-                "Calendar",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.black),
+              TableCalendar(
+                firstDay: _initialCalendarDate,
+                lastDay: _lastCalendarDate,
+                focusedDay: _focusedCalendarDate,
+                calendarFormat: CalendarFormat.month,
+                startingDayOfWeek: StartingDayOfWeek.monday,
+                daysOfWeekHeight: 40,
+                rowHeight: 50,
+                eventLoader: _listOfDayEvents,
+                calendarStyle: CalendarStyle(
+                  weekNumberTextStyle: dayStyle(FontWeight.normal),
+                  weekendTextStyle: dayStyle(FontWeight.normal),
+                  // selectedTextStyle: const TextStyle(color: Colors.black),
+                  todayTextStyle: const TextStyle(color: Colors.white),
+                  todayDecoration: const BoxDecoration(
+                      color: Colors.grey, shape: BoxShape.circle),
+                  selectedDecoration: const BoxDecoration(
+                      color: Colors.black, shape: BoxShape.circle),
+                ),
+                daysOfWeekStyle: const DaysOfWeekStyle(
+                  weekdayStyle: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                  weekendStyle: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                ),
+                headerStyle: const HeaderStyle(
+                    formatButtonVisible: false,
+                    titleTextStyle: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25),
+                    leftChevronIcon:
+                        Icon(Icons.chevron_left, color: Colors.black),
+                    rightChevronIcon: Icon(
+                      Icons.chevron_right,
+                      color: Colors.black,
+                    )),
+                selectedDayPredicate: (currentSelectedDate) {
+                  return (isSameDay(
+                      selectedCalendarDate!, currentSelectedDate));
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  // as per the documentation
+                  if (!isSameDay(selectedCalendarDate, selectedDay)) {
+                    setState(() {
+                      selectedCalendarDate = selectedDay;
+                      _focusedCalendarDate = focusedDay;
+                    });
+                  }
+                },
               ),
-              Text(
-                DateFormat('MMMM yyyy').format(_selectedDate),
-                style: const TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 15,
-                    color: Colors.grey),
-              )
+              // ..._listOfDayEvents(selectedCalendarDate!).map(
+              //   (myEvents) => ListTile(
+              //     leading: const Icon(
+              //       Icons.done,
+              //       color: Colors.black,
+              //     ),
+              //     title: Padding(
+              //       padding: const EdgeInsets.only(bottom: 8.0),
+              //       child: Text('Event Title:   ${myEvents.eventTitle}'),
+              //     ),
+              //     subtitle: Text('Description:   ${myEvents.eventDescp}'),
+              //   ),
+              // ),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.only(left: 20),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.55,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
+                ),
+                child: Stack(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(top: 20),
+                          child: Text(
+                            'Today',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        taskList('Task 1', 'This is a description of task 1',
+                            Icons.check_box, Colors.black)
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 50,
-              child: GridView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 7,
-                  ),
-                  itemCount: daysOfWeek.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      alignment: Alignment.center,
-                      child: Text(
-                        daysOfWeek[index],
-                        style: const TextStyle(
-                            fontSize: 15,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    );
-                  }),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
-                ),
-              ),
-              child: GridView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                padding: EdgeInsets.zero,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 7,
-                ),
-                itemCount: listOfDatesInMonth(_selectedDate).length +
-                    indexOfFirstDayMonth,
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: GestureDetector(
-                      onTap: () => index >= indexOfFirstDayMonth
-                          ? setState(() {
-                              _selectedIndex = index;
-                            })
-                          : null,
-                      child: Container(
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                            color: index == _selectedIndex
-                                ? Colors.black
-                                : Colors.transparent,
-                            borderRadius: BorderRadius.circular(50)),
-                        child: index < indexOfFirstDayMonth
-                            ? const Text("")
-                            : Text('${index + 1 - indexOfFirstDayMonth}',
-                                style: TextStyle(
-                                    color: index == _selectedIndex
-                                        ? Colors.white
-                                        : index % 7 == 6
-                                            ? Colors.grey
-                                            : Colors.black,
-                                    fontSize: 17)),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            const SizedBox(height: 10),
-            Expanded(
-                child: Column(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  padding: const EdgeInsets.only(bottom: 20, top: 10),
-                  alignment: Alignment.center,
-                  child: const Text("No events today",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
-                ),
-              ],
-            )),
-          ],
-        ),
       ),
-    );
+    ));
   }
 }
 
-List<int> listOfDatesInMonth(DateTime currentDate) {
-  var selectedMonthFirstDay =
-      DateTime(currentDate.year, currentDate.month, currentDate.day);
-  var nextMonthFirstDay = DateTime(selectedMonthFirstDay.year,
-      selectedMonthFirstDay.month + 1, selectedMonthFirstDay.day);
-  var totalDays = nextMonthFirstDay.difference(selectedMonthFirstDay).inDays;
+class MyEvents {
+  final String eventTitle;
+  final String eventDescp;
 
-  var listOfDates = List<int>.generate(totalDays, (i) => i + 1);
-  return (listOfDates);
+  MyEvents({required this.eventTitle, required this.eventDescp});
+
+  @override
+  String toString() => eventTitle;
 }
-
-int getIndexOfFirstDayInMonth(DateTime currentDate) {
-  var selectedMonthFirstDay =
-      DateTime(currentDate.year, currentDate.month, currentDate.day);
-  var day = DateFormat('EEE').format(selectedMonthFirstDay).toUpperCase();
-
-  return daysOfWeek.indexOf(day) - 1;
-}
-
-final List<String> daysOfWeek = [
-  "MON",
-  "TUE",
-  "WED",
-  "THU",
-  "FRI",
-  "SAT",
-  "SUN",
-];
